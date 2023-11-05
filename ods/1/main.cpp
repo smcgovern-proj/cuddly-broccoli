44949f4c2291176466c3829091282b7413e79bff
Sean McGovern <smcgovern135@gmail.com>
2023-11-05 16:24:59 -0600

    ex 1.1.2

--- ods/1/main.cpp
+++ ods/1/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <ostream>
 #include <string>
 #include <stack>
 
+void log_stack(std::stack<std::string>& s) {
+	while (!s.empty()) {
+		std::cout << s.top() + "\n";
+		s.pop();
+	}
+}
+
 void ex_1() {
-	std::ifstream inf{ "./sample_data.txt" };
+	std::ifstream inf{ "./sample_data_1.1.1.txt" };
 	
 	if (!inf) {
 		std::cerr << "Could not open file";
@@ -17,15 +25,44 @@ void ex_1() {
 		std::getline(inf, line);
 		s.push(line);
 	}
-
-	while (!s.empty()) {
-		std::cout << s.top() + "\n";
-		s.pop();
-	}
+	
+	log_stack(s);
 }
 
+void ex_2() {
+	std::ifstream inf{ "./sample_data_1.1.2.txt" };
+	
+	if (!inf) {
+		std::cerr << "Could not open file";
+	}
+
+	std::stack<std::string> s{};
+	
+	int count{ 0 };
+	while (inf) {
+		if (count <= 49) {
+			std::string line{};
+			std::getline(inf, line);
+			if (line.length() > 0) {
+				s.push(line);
+			}
+			count++;
+			continue;
+		} 
+		log_stack(s);		
+		count = 0;
+	}
+	log_stack(s);
+}
+
+void ex_3() {
+	
+}
+
+
 int main() {
-	ex_1();
+	// ex_1();
+	ex_2();
 	return 0;
 }
 
--- ods/1/populate_data.sh
+++ ods/1/populate_data.sh
@@ -3,9 +3,22 @@
 set -e
 set -u
 
-touch ./sample_data.txt
-for value in {1..50}
-do
-	echo "line $value" >> ./sample_data.txt
-done
+populate_1.1.1 () {
+	touch ./sample_data_1.1.1.txt
+	for value in {1..50}
+	do
+		echo "line $value" >> ./sample_data_1.1.1.txt
+	done
+}
+
+populate_1.1.2 () {
+	touch ./sample_data_1.1.2.txt
+	for value in {1..103}
+	do
+		echo "line $value" >> ./sample_data_1.1.2.txt
+	done
+}
+
+populate_1.1.1
+populate_1.1.2
 
